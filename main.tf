c49e8c0c831a15f065a3405eb48615a1e2c01631
user1gh1 <user1gm1@gmail.com>
2023-10-05 03:12:23 +0400

    l

--- main.tf
+++ main.tf
@@ -17,7 +17,25 @@ resource "aws_internet_gateway" "my_internet_gateway" {
     Name = "InternetGateway"
   }
 }
+#---------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.myvpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.publicsubnet.id
+  route_table_id = aws_route_table.public.id
+}
+#---------------
 resource "aws_instance" "server" {
   ami                    = data.aws_ami.latest_free_ami.id
   instance_type          = var.ec2_instance_type
